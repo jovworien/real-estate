d0cf04a1c244854547b6c1d1222dfe439b89f60d
Ese-oghene <oghenee08@gmail.com>
2024-12-25 00:27:43 +0100

    productgrid

--- src/app/propertyGrid/contact-button/contact-button.component.ts
+++ src/app/propertyGrid/contact-button/contact-button.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './contact-button.component.css'
 })
 export class ContactButtonComponent {
+ products = [
+    {
+      name: 'Chair',
+      image: '../images/productgrid/house1.jpeg',
+      ratingValue: 4,
+      rating: Array(5),
+      price: 1000,
+      isLoved: false
+    },
+    {
+      name: 'Bed',
+      image: '../images/productgrid/house2.jpeg',
+    },
+    {
+      name: 'House Items',
+      image: '../images/productgrid/house3.jpeg',
+      isLoved: false
+    }, 
+
+  ];
+
 
 }
